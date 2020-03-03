bd8d11cf0dbbfe1d8e6a37ef4cd8c4aa1ef1b8da
Jack Gonser <TheKiller65YT@users.noreply.github.com>
2020-03-02 16:29:07 -1000

    Add individually addressable LEDs

--- SRC/LEDStrip/LEDStrip.ino
+++ SRC/LEDStrip/LEDStrip.ino
@@ -3,68 +3,74 @@
  * This code processes the values and writes the respective color values to the LED strip.
  */
 
+//TODO: Add programable "sectors" for LEDs in multiple locations
+
 #include <Wire.h> //For communicating over I2C.
 #include <FastLED.h> //For writing to the LED strip.
 
-const int ledStrip; //TBD which pin this will be on.
-const int I2CAddress = 5;
+const int I2CAddress = 21;
 
-unsigned int I2CValues[50]; //Array to hold the three RGB values sent over I2C.
+unsigned int I2CValues[10]; //Array to hold the three RGB values sent over I2C.
 unsigned int RGBValues; //Temporary storage variable to hold the value of Wire.Read(); before being placed in I2CValues[]
+unsigned int ledNum[2] = {0, 179};
 
 //These booleans verify that the recieved bytes are processed correctly.
-bool receiveAll1 = false;
-bool receiveAll2 = false;
-bool receiveAll3 = false;
+bool receive[5] = {false, false, false, false, false};
 
-// SETTINGS FOR LEDS -- Reminder: Revise these values later. It is not yet certain if these are correct.
-#define DATA_PIN    2
-//#define CLK_PIN   4
-#define LED_TYPE    WS2812
-#define COLOR_ORDER GRB //Note: The color order here is GRB, but below in the loop the CRGB write function orders the values in RGB. Should the order be changed here or in the write function? Or does it automatically accommodate for this issue? -Braden.
-#define NUM_LEDS    26
-CRGB leds[NUM_LEDS];
+#define CHIPSET WS2812B
+#define PIN     12
+#define COLOR_ORDER GRB
+
+//Individual numbers define the length of each strand
+const unsigned int LED_COUNT = 122+28+29;
+
+CRGB leds[LED_COUNT];
 
 void setup() {
-  // put your setup code here, to run once:
-  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050); //Reminder: Revise these values later. It is not yet certain if these are correct.
+  // put your setup code here, to run once
   Wire.begin(I2CAddress);
   Wire.onReceive(receiveEvent);
   Serial.begin(9600);
-  FastLED. //Note: This was just left hanging here? Is it unfinished code or can I delete it. -Braden.
+  FastLED.addLeds<CHIPSET, PIN, COLOR_ORDER>(leds, LED_COUNT);
 }
 
 void loop() {
-  if (recieveAll3) {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB(I2CValues[0], I2CValues[1], I2CValues[2]); //Turns on the LEDs at the specified colors.
-      bool receiveAll1 = false;
-      bool receiveAll2 = false;
-      bool receiveAll3 = false;
-    }
+  if (receive[4]) {
+    FastLED.clear();
+    for (int i = ledNum[0]; i < ledNum[1]; i++) leds[i] = CRGB(I2CValues[0], I2CValues[1], I2CValues[2]);
+    FastLED.show();
+    for (int i = 0; i < 5; i++) receive[i] = false;
   }
 }
 
 //Runs when data is received.
 void receiveEvent() {
-  //while (Wire.available()) {
-    if (receiveAll1 == false)
+  while (Wire.available()) {
+    if (receive[0] == false)
     {
-      receiveAll1 = true;
+      receive[0] = true;
       RGBValues = Wire.read();
       I2CValues[0] = RGBValues;
     }
-    else if (receiveAll1 == true && receiveAll2 == false)
+    else if (receive[0] == true && receive[1] == false)
     {
-      receiveAll2 = true;
+      receive[1] = true;
       RGBValues = Wire.read();
       I2CValues[1] = RGBValues;
+    } else if (receive[1] == true && receive[2] == false)
+    {
+      receive[2] = true;
+      RGBValues = Wire.read();
+      I2CValues[2] = RGBValues;
+    } else if (receive[2] == true && receive[3] == false)
+    {
+      receive[3] = true;
+      ledNum[0] = Wire.read();
     }
     else
     {
-      receiveAll3 = true;
-      RGBValues = Wire.read();
-      I2CValues[2] = RGBValues;
+      receive[4] = true;
+      ledNum[1] = Wire.read();
     }
-  //}
+  }
 }
